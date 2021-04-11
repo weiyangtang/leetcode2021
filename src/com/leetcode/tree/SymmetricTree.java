279e1dce9f64c31593a05b79cf9e91b31f6c69eb
tangweiyang <tangweiyang@baijia.com>
2021-04-11 17:32:56 +0800

    判断是否镜像对称，simple

---
+++ src/com/leetcode/tree/SymmetricTree.java
@@ -0,0 +1,51 @@
+package com.leetcode.tree;
+
+/**
+ * @author tangweiyang
+ * @Description:
+ * @date 2021/4/11 下午5:31
+ *
+ * 101. 对称二叉树
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ *
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * https://leetcode-cn.com/problems/symmetric-tree/
+ */
+public class SymmetricTree {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return dfs(root.left, root.right);
+    }
+
+    private boolean dfs(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        if ((root1 != null && root2 == null) || (root1 == null && root2 != null)
+            || root1 != null && root2 != null && root1.val != root2.val) {
+            return false;
+        }
+        return dfs(root1.left, root2.right) && dfs(root1.right, root2.left);
+    }
+}
